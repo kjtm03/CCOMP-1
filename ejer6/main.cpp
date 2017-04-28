ebfbe834be528265af447c37a210ab31bf255ae0
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-04-28 10:34:29 -0500

    tarea

--- ejer6/main.cpp
+++ ejer6/main.cpp
@@ -4,28 +4,19 @@ using namespace std;
 
 int main()
 {
-    string a;
-    int b,c;
-    cout<<"Escribe la operacion: \n";
-    cin>>a;
-    cout<<"Escribe el primer numero: \n";
-    cin>>b;
-    cout<<"Escribe el segundo numero: \n";
-    cin>>c;
-    if (a=="/")
-        {
-            cout<<"La division es: "<<b/c<<endl;
-        }
-    if (a=="*")
-        {
-            cout<<"La multiplicacion es: "<<b*c<<endl;
-        }
-    if (a=="-")
-        {
-            cout<<"La resta es: "<<b-c<<endl;
-        }
-    if (a=="+")
-        {
-            cout<<"La suma es: "<<b+c<<endl;
-        }
+//EJERCICIO 6
+ char o; float a,b,resul;
+    cout<<"Ingrese el operador y dos operandos\n"; cin>>o>>a>>b;
+    if(o=='+')
+        resul=a+b;
+    else if(o=='-')
+        resul=a-b;
+    else if(o=='*')
+        resul=a*b;
+    else if(o=='/')
+        resul=a/b;
+
+    cout<<a<<o<<b<<" es: "<<resul;
+
+    return 0;
 }
