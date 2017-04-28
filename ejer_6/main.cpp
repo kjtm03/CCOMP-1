a02f0ffb09e0c75dbad28536a8216d9ab25b5185
kjtm03 <jktm01@gmail.com>
2017-04-27 20:03:01 -0500

    tarea

---
+++ ejer_6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+char funcion(char operacion){
+    char arr[] = {operacion};
+    char posicion = arr[0];
+    for(int i = 0; posicion != '\0'; posicion = arr[i++]){
+        if(arr[0]='+'){
+
+        }
+
+    }
+}
+int main()
+{
+ char x;
+ cin>>x;
+ cout<<funcion(x)<<endl;
+}
